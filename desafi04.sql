6181d103f8d7abe3997c553c3d30e694fcb066d1
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-07-01 05:58:58 -0300

    fix: desafio 4 completo

--- desafi04.sql
+++ desafi04.sql
@@ -1,5 +1,6 @@
-SELECT u.user_name AS usuario, IF(YEAR(reproduction_date) = 2021, 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
+SELECT u.user_name AS usuario, IF(MAX(YEAR(reproduction_date)) = 2021, 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
 FROM SpotifyClone.user AS u
 INNER JOIN SpotifyClone.user_song AS us
 ON u.user_id=us.user_id 
+GROUP BY u.user_name
 ORDER BY u.user_name ASC;
