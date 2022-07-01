47b75b9a212dd6d1beefcb50a4cf0560161bc951
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-07-01 07:10:26 -0300

    fix: desafio 6 ROUND alterado para TRUNCATE, tudo em uma linha

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,1 @@
-SELECT MIN(s.subscription_price) AS faturamento_minimo, MAX(s.subscription_price) AS faturamento_maximo, ROUND(AVG(subscription_price), 2) AS faturamento_medio, ROUND(SUM(subscription_price), 2) AS faturamento_total
-FROM SpotifyClone.subscription AS s
-INNER JOIN SpotifyClone.user AS u
-ON s.subscription_id=u.subscription;
+SELECT MIN(s.subscription_price) AS faturamento_minimo, MAX(s.subscription_price) AS faturamento_maximo, ROUND(AVG(subscription_price), 2) AS faturamento_medio, TRUNCATE(SUM(subscription_price), 2) AS faturamento_total FROM SpotifyClone.subscription AS s INNER JOIN SpotifyClone.user AS u ON s.subscription_id=u.subscription;
