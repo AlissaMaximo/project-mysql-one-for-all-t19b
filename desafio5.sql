b6d57db66cfc427cbea8cf7d9ec7d7026653b7fd
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-07-01 06:23:22 -0300

    feat: desafio 5 feito

--- desafio5.sql
+++ desafio5.sql
@@ -1,1 +1,8 @@
-#desafio 5
+SELECT song_name AS cancao, COUNT(us.song_id) AS reproducoes
+FROM SpotifyClone.user AS u
+INNER JOIN SpotifyClone.user_song AS us
+ON u.user_id=us.user_id 
+INNER JOIN SpotifyClone.song AS s
+ON s.song_id=us.song_id
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao ASC LIMIT 2;
